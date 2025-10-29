ca92dbf39857e41e89596a88e4e8e0b87a740534
Rishab-rt <rtalusani07@gmail.com>
2025-10-29 11:18:47 -0400

    finished hw2

--- hw2/boilerplate/problem1.js
+++ hw2/boilerplate/problem1.js
@@ -28,6 +28,13 @@ const taskCases = require('../data/tasks-cases.json'); // imports taskCases arra
 /* Write your function here */
 const getAllTasks = (req, res) => {
   // TODO: implement this function
+  for (let i = 0; i < taskCases.length; i++)
+  {
+    if (taskCases[i].id===req.id)
+    {
+      return {data: taskCases[i]};
+    }
+  }
   throw new Error('Not implemented');
 };
 
